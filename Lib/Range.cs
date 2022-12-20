0541b6045fa48cb4609ff70f112ebffae0d1d6a6
Benjamin Sutas <benjamin.sutas@wisetechglobal.com>
2022-12-20 11:49:48 +1100

    initial

---
+++ Lib/Range.cs
@@ -0,0 +1,29 @@
+﻿namespace Lib
+{
+	public struct TimeOnly
+	{
+		public TimeOnly(int hour, int minute, int second) => underlying = new DateTime(1, 1, 1, hour, minute, second);
+
+		public int Hour => underlying.Hour;
+		public int Minute => underlying.Minute;
+		public int Second => underlying.Second;
+
+		DateTime underlying;
+	}
+
+	public struct Range<T> where T : struct
+	{
+		// this static property is the bug - comment this out and nUnit loads this type correctly
+		public static readonly Range<TimeOnly> NineToFive = new(new TimeOnly(9, 0, 0), new TimeOnly(17, 0, 0));
+
+		public T Start { get; private set; }
+
+		public T End { get; private set; }
+
+		public Range(T start, T end)
+		{
+			Start = start;
+			End = end;
+		}
+	}
+}
---
+++ Test/FailingTest.cs
@@ -0,0 +1,19 @@
+﻿using Lib;
+using NUnit.Framework;
+using TimeOnly = Lib.TimeOnly;
+
+namespace Error
+{
+	[TestFixture]
+	public class RangeTests
+	{
+		[Test]
+		public void TestFail()
+		{
+			var expected = new Range<TimeOnly>(new TimeOnly(12, 0, 0), new TimeOnly(12, 3, 1));
+			var actual = new Range<TimeOnly>(new TimeOnly(12, 0, 0), new TimeOnly(12, 3, 1));
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
